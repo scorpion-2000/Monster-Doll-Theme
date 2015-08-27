38cc3244e5ab7f65502968a2fd5148ab2d6ef960
scorpion-2000 <victorscorpion2000@gmail.com>
2015-08-27 09:32:23 +0300

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Monster Doll Theme',
+    'description': 'This module provides feature for changing layout of theme.',
+    'summary': 'This module provides feature for changing layout of theme.',
+    'category': 'Theme',
+    'version': '1.0',
+    'website': 'http://www.monster-doll.kiev.ua',
+    'author': 'Victor Scorpion',
+    'depends': ['website'],
+    'data': [
+        'static/src/views/theme.xml',
+    ],
+    'images':[
+            'static/description/splash_screen.png',
+    ],
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
